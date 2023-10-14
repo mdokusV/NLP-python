24df990a737d847ed1021ba8c686f905b44d3b10
HOME-VM-TOSCHOOL <mat.dok123@gmial.com>
2023-10-14 20:52:42 +0200

    Proper File Creation

--- zaj1/A01.py
+++ zaj1/A01.py
@@ -4,16 +4,23 @@ import array
 import random
 import calendar
 from datetime import datetime, timedelta
-
+import os
 
 
 NUMBER_OF_INVOICES = 10
+DIR = os.path.dirname(__file__)
+print(DIR)
 
 first_names = ("John", "Andy", "Joe", "Jane", "Mary", "Peter", "Linda")
 last_names = ("Johnson", "Smith", "Williams", "Jones", "Brown", "Taylor", "Thomas")
 
+
+def join_path(filename: str) -> str:
+    return os.path.join(DIR, filename)
+
+
 # put people into a csv
-with open("employee.csv", "w", newline="") as csvfile:
+with open(join_path("employee.csv"), "w", newline="") as csvfile:
     fileReader = csv.writer(
         csvfile, delimiter=" ", quotechar="|", quoting=csv.QUOTE_MINIMAL
     )
@@ -32,7 +39,7 @@ def generate_date(x: int) -> datetime.date:
 
 
 # put ReceivedInvoice into a csv
-with open("received.csv", "w", newline="") as csvfile:
+with open(join_path("received.csv"), "w", newline="") as csvfile:
     fileReader = csv.writer(
         csvfile, delimiter=" ", quotechar="|", quoting=csv.QUOTE_MINIMAL
     )
@@ -47,11 +54,11 @@ with open("received.csv", "w", newline="") as csvfile:
         iter += 1
 
 # put IssuedInvoice into a csv
-with open("issued.csv", "w", newline="") as csvfile:
+with open(join_path("issued.csv"), "w", newline="") as csvfile:
     fileReader = csv.writer(
         csvfile, delimiter=" ", quotechar="|", quoting=csv.QUOTE_MINIMAL
     )
-    iter = 2 
+    iter = 2
     for x in range(NUMBER_OF_INVOICES):
         fileReader.writerow(
             [
@@ -62,25 +69,31 @@ with open("issued.csv", "w", newline="") as csvfile:
         iter += 1
 
 # read people from a csv
-with open("employee.csv", newline="") as csvfile:
+with open(join_path("employee.csv"), newline="") as csvfile:
     fileReader = csv.reader(csvfile, delimiter=" ", quotechar="|")
     people = []
     for row in fileReader:
         people.append(classes.Employee(row[0], row[1], float(row[2])))
 
 # read ReceivedInvoice from a csv
-with open("received.csv", newline="") as csvfile:
+with open(join_path("received.csv"), newline="") as csvfile:
     fileReader = csv.reader(csvfile, delimiter=" ", quotechar="|")
     receivedInvoices = []
     for row in fileReader:
-        receivedInvoices.append(classes.ReceivedInvoice(datetime.strptime(row[0], "%Y-%m-%d"), float(row[1])))
+        receivedInvoices.append(
+            classes.ReceivedInvoice(
+                datetime.strptime(row[0], "%Y-%m-%d"), float(row[1])
+            )
+        )
 
 # read IssuedInvoice from a csv
-with open("issued.csv", newline="") as csvfile:
+with open(join_path("issued.csv"), newline="") as csvfile:
     fileReader = csv.reader(csvfile, delimiter=" ", quotechar="|")
     issuedInvoices = []
     for row in fileReader:
-        issuedInvoices.append(classes.IssuedInvoice(datetime.strptime(row[0], "%Y-%m-%d"), float(row[1])))
+        issuedInvoices.append(
+            classes.IssuedInvoice(datetime.strptime(row[0], "%Y-%m-%d"), float(row[1]))
+        )
 
 
 employmentCost = 0
@@ -97,7 +110,7 @@ for person in people:
 budget = []
 print("month    ", "income")
 for i in range(len(issuedInvoices)):
-    budget.append(issuedInvoices[i].amount - receivedInvoices[i].amount - employmentCost)
-    print(calendar.month_name[issuedInvoices[i].date.month-1],":", budget[i])
-
-
+    budget.append(
+        issuedInvoices[i].amount - receivedInvoices[i].amount - employmentCost
+    )
+    print(calendar.month_name[issuedInvoices[i].date.month - 1], ":", budget[i])
